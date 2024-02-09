88c1864f5f9c0fb5b76bba18ee61f3a7269a3caf
B-Step62 <yuki.watanabe@databricks.com>
2024-02-09 15:56:30 +0900

    Add docstring for new param save_pretrained
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- mlflow/transformers/__init__.py
+++ mlflow/transformers/__init__.py
@@ -449,6 +449,7 @@ def save_model(
                                     release without warning.
         example_no_conversion: {{ example_no_conversion }}
         prompt_template: {{ prompt_template }}
+        save_pretrained: {{ save_pretrained }}
         kwargs: Optional additional configurations for transformers serialization.
 
     """
@@ -854,6 +855,7 @@ def log_model(
                                     release without warning.
         example_no_conversion: {{ example_no_conversion }}
         prompt_template: {{ prompt_template }}
+        save_pretrained: {{ save_pretrained }}
         kwargs: Additional arguments for :py:class:`mlflow.models.model.Model`
     """
     return Model.log(
