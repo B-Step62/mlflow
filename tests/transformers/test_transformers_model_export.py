7e0e5353824a00ab67d62bb767d2ea6ea34fe4c6
B-Step62 <yuki.watanabe@databricks.com>
2024-02-12 00:07:34 +0900

    Add persist_pretrained_model API for Transformers flavor
    The API is primary for updating an MLflow model that was logged/saved
    with setting save_pretrained=False, so it can be registered to the
    Model Registry.
    
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- tests/transformers/test_transformers_model_export.py
+++ tests/transformers/test_transformers_model_export.py
@@ -5,6 +5,7 @@ import json
 import os
 import pathlib
 import textwrap
+from pathlib import Path
 from unittest import mock
 
 import huggingface_hub
@@ -3619,3 +3620,33 @@ def test_save_and_load_pipeline_without_save_pretrained_false(
         if hasattr(input_example, "_pytestfixturefunction"):
             input_example = request.getfixturevalue(input_example.__name__)
         mlflow.pyfunc.load_model(model_path).predict(input_example)
+
+
+def test_persist_pretrained_model(small_seq2seq_pipeline):
+    with mlflow.start_run():
+        model_info = mlflow.transformers.log_model(
+            transformers_model=small_seq2seq_pipeline,
+            artifact_path="model",
+            save_pretrained=False,
+            pip_requirements=["mlflow"],  # For speed up logging
+        )
+
+    artifact_path = Path(mlflow.artifacts.download_artifacts(model_info.model_uri))
+    model_path = artifact_path / "model"
+    tokenizer_path = artifact_path / "components" / "tokenizer"
+
+    original_config = Model.load(artifact_path).flavors["transformers"]
+    assert "model_binary" not in original_config
+    assert "source_model_revision" in original_config
+    assert not os.path.exists(model_path)
+    assert not os.path.exists(tokenizer_path)
+
+    mlflow.transformers.persist_pretrained_model(model_info.model_uri)
+
+    updated_config = Model.load(model_info.model_uri).flavors["transformers"]
+    assert "model_binary" in updated_config
+    assert "source_model_revision" not in updated_config
+    assert os.path.exists(model_path)
+    assert os.path.exists(model_path / "tf_model.h5")
+    assert os.path.exists(tokenizer_path)
+    assert os.path.exists(tokenizer_path / "tokenizer.json")
